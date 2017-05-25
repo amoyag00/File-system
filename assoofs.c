e3cfdf9bfd966b6b3934ec758258e90f7eb31de1
amoyag00 <amoyag00@estudiantes.unileon.es>
2017-05-25 20:22:40 +0200

    Functions iterate and lookup implemented. Also made 2 auxiliar methods for getting an inode and getting a inode_info struct

--- assoofs.c
+++ assoofs.c
@@ -19,6 +19,8 @@ static int assoofs_mkdir(struct inode *dir , struct dentry *dentry , umode_t mod
 ssize_t  assoofs_read(struct  file *filp , char  __user *buf , size_t len , loff_t *ppos);
 ssize_t  assoofs_write(struct  file *filp , const  char  __user *buf , size_t len , loff_t *ppos);
 static  int  assoofs_iterate(struct  file *filp , struct  dir_context *ctx);
+struct assoofs_inode_info *assoofs_get_inode_info(struct super_block *sb, uint64_t ino);
+struct inode *assoofs_get_inode(struct super_block *sb, uint64_t ino);
 
 
 static  struct  file_system_type  assoofs_type = {
@@ -52,8 +54,8 @@ static  const  struct  super_operations  assoofs_sops = {//Hay que añadir más 
 static  int  __init  assoofs_init(void)
 {
 	 if(register_filesystem(&assoofs_type)!=0){
-	 		printk("No se ha registrado correctamente\n");
-	 		return -1;
+	 	printk("No se ha registrado correctamente\n");
+	 	return -1;
 	 }else{
 	 	printk("Registrado correctamente\n");
 	 }
@@ -84,7 +86,6 @@ int assoofs_fill_super(struct super_block *sb, void *data , int silent)
 	struct buffer_head *buff;
 	struct assoofs_super_block_info *asb;
 	struct inode *root_inode;
-	struct assoofs_inode_info *info;
 
 	//Leer información del superbloque
 	buff=sb_bread(sb,ASSOOFS_SUPERBLOCK_BLOCK_NUMBER);//El super bloque está en el bloque 0
@@ -106,7 +107,7 @@ int assoofs_fill_super(struct super_block *sb, void *data , int silent)
 	sb->s_maxbytes=ASSOOFS_DEFAULT_BLOCK_SIZE;
 	sb->s_fs_info=asb;//Guardamos el superbloque_info porque probablemente lo necesitaremos luego.
 	sb->s_op=&assoofs_sops;//direccion de variable tipo super_operation que será global. Operaciones que soporta el superbloque
-	printk("Superbloque creado\n");
+	printk("Superbloque- número mágico %lu\n",sb->s_magic);
 
 	//Crear el inodo raíz
 	root_inode=new_inode(sb);
@@ -117,24 +118,86 @@ int assoofs_fill_super(struct super_block *sb, void *data , int silent)
 	root_inode->i_mtime=CURRENT_TIME;
 	root_inode->i_ctime=CURRENT_TIME;
 	root_inode->i_op=&assoofs_inode_ops;
-	root_inode->i_fop=&assoofs_file_operations;
-
-	info->mode=root_inode->i_mode;
-	info->inode_no=ASSOOFS_ROOTDIR_INODE_NUMBER;
-	info->data_block_number=ASSOOFS_ROOTDIR_DATABLOCK_NUMBER;
-	info->file_size=root_inode->i_size;
-	info->dir_children_count=0;
-	root_inode->i_private= info;//el inode_info, similar al asb en  s_fs_info
-
+	root_inode->i_fop=&assoofs_dir_operations;
+	root_inode->i_private= assoofs_get_inode_info(sb,ASSOOFS_ROOTDIR_INODE_NUMBER);//el inode_info, similar al asb en  s_fs_info
+	
 	sb->s_root=d_make_root(root_inode);
 	printk("inodo raíz creado\n");
+
+
 	return 0;
 
 }
 
-struct dentry *assoofs_lookup(struct inode *parent_inode , struct dentry *child_dentry , unsigned int flags){
+struct assoofs_inode_info *assoofs_get_inode_info(struct super_block *sb,uint64_t ino){
+	int i=0;
+	struct buffer_head *buffer;
+	struct assoofs_inode_info *ino_info;
+	struct assoofs_super_block_info *sb_info;
+	buffer=sb_bread(sb,ASSOOFS_INODESTORE_BLOCK_NUMBER);
+	sb_info=sb->s_fs_info;
+	ino_info=(struct assoofs_inode_info *)buffer->b_data;
+	for (i=0;i<sb_info->inodes_count;i++){
+		if(ino==ino_info->inode_no){
+			brelse(buffer);
+			return ino_info;
+		}
+		ino_info++;
+	}
+	return NULL;
+}
 
-	printk("inodo encontrado\n");
+struct inode *assoofs_get_inode(struct super_block *sb, uint64_t ino){
+	struct assoofs_inode_info *ino_info;
+	struct inode *inode;
+	ino_info=assoofs_get_inode_info(sb,ino);
+	inode= new_inode(sb);
+	inode->i_sb=sb;
+	inode->i_op=&assoofs_inode_ops;
+	inode->i_atime=CURRENT_TIME;
+	inode->i_ctime=CURRENT_TIME;
+	inode->i_mtime=CURRENT_TIME;
+	inode->i_private=ino_info;
+	inode->i_ino=ino;
+	if(S_ISDIR(ino_info->mode)){
+		inode->i_fop=&assoofs_dir_operations;
+	}else if (S_ISREG(ino_info->mode)){
+		inode->i_fop=&assoofs_file_operations;
+	}
+	//Después de llamar a esta función hay que llamar a init_owner diciéndole el inodo padre.
+	//Aquí no podemos porque no sabemos cuál es.
+	return inode;
+
+}
+
+struct dentry *assoofs_lookup(struct inode *parent_inode , struct dentry *child_dentry , unsigned int flags){
+	struct super_block *sb;
+	struct buffer_head *buffer;
+	struct assoofs_dir_record_entry *record;
+	struct inode *inode;
+	struct assoofs_inode_info *ino_info,*ino_info2;
+	struct qstr dname;
+	int i=0;
+	sb=parent_inode->i_sb;
+	ino_info=parent_inode->i_private;
+	buffer=sb_bread(sb,ino_info->data_block_number);
+	record=(struct assoofs_dir_record_entry *)buffer->b_data;
+	
+	for(i=0;i<ino_info->dir_children_count;i++){
+		dname=child_dentry->d_name;
+		if(strcmp(dname.name,record->filename)==0){
+			inode=assoofs_get_inode(sb,record->inode_no);
+			ino_info2=inode->i_private;
+			inode_init_owner(inode,parent_inode,ino_info2->mode);
+			d_add(child_dentry,inode);//Esto lo mantiene en memoria
+			brelse(buffer);
+			printk("inodo encontrado\n");
+			return NULL;
+		}
+		record++;
+
+	}
+	
 	return NULL;
 }
 
@@ -149,6 +212,10 @@ static int assoofs_mkdir(struct inode *dir , struct dentry *dentry , umode_t mod
 }
 
 ssize_t  assoofs_read(struct  file *filp , char  __user *buf , size_t len , loff_t *ppos){
+	//Leer el bloque
+	//DE filp se saca el numero de inodo
+	//guardar lo leido (buffer_head) en buf
+	//usar copy_to_user
 	printk("Se han leído %lu bytes\n",len);
 	return 0;
 }
@@ -159,10 +226,35 @@ ssize_t  assoofs_write(struct  file *filp , const  char  __user *buf , size_t le
 }
 
 static  int  assoofs_iterate(struct  file *filp , struct  dir_context *ctx){
-	printk("Iterate\n");
+	struct inode *inode;
+	struct buffer_head *buffer;
+	struct assoofs_dir_record_entry *record;
+	struct super_block *sb;
+	struct assoofs_inode_info *ino_info;
+	int i=0;
+	inode=filp->f_path.dentry->d_inode;
+	sb=inode->i_sb;
+	ino_info=inode->i_private;
+	if(ctx->pos){
+		return 0;
+	}
+	buffer=sb_bread(sb,ino_info->data_block_number);
+	record=(struct assoofs_dir_record_entry*)buffer->b_data;
+	printk("Dir children count %llu \n ",ino_info->dir_children_count);
+	for (i=0;i<ino_info->dir_children_count;i++){
+		printk("Iteración: %d",i);
+		dir_emit(ctx,record->filename,ASSOOFS_FILENAME_MAXLEN,record->inode_no,DT_UNKNOWN);//Actualiza el contexto
+		ctx->pos+=sizeof(struct assoofs_dir_record_entry);
+		record++;
+	}
+	brelse(buffer);
+	printk("Iterated\n");
 	return 0;
 
 }
 
 module_init(assoofs_init);
 module_exit(assoofs_exit);
+
+
+
